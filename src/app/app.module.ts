e0f8b271ce22324ce1deedf1590e12b52d0f3f74
josaphat mayuba <mayubandele@live.fr>
2017-05-15 06:34:19 -0400

    del layout no use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthGuard } from './shared';
 
+
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: Http) {
     return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
